4213050e8f129991823dc32541735b3bea444e5c
Ori <ori.levit@gmail.com>
2023-01-05 12:38:41 +0200

    Added addWidgets function to init

--- loginWindow.py
+++ loginWindow.py
@@ -12,6 +12,7 @@ SCREEN_HEIGHT = 300
 class LoginWindow(CustomWindow.Window):
     def __init__(self):
         super().__init__()
+        self.add_widgets()
 
     def add_widgets(self):
         # region main frame
